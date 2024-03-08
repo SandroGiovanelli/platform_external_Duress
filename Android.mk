1d9202b3665c536928eeb9daa6e6ad334c34d5f4
SandroGiovanelli <162636904+SandroGiovanelli@users.noreply.github.com>
2024-03-08 21:56:57 +0100

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,11 +1,21 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := app.me.lucky.duress.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_TAGS := optional
+LOCAL_PRODUCT_MODULE := true
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT_ETC)/permissions
+LOCAL_SRC_FILES := permissions/$(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
 LOCAL_MODULE := Duress
 LOCAL_MODULE_CLASS := APPS
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_PRODUCT_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := prebuilt/Duress.apk
-LOCAL_PRODUCT_MODULE := true
 
+LOCAL_REQUIRED_MODULES := app.me.lucky.duress.xml
 include $(BUILD_PREBUILT)
